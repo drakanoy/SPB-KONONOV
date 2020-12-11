1edbba74e17adafd15e3142823e42cc3767f7cf6
drakanoy <drakanoy@gmail.com>
2020-12-11 16:19:42 +0300

    красивый пайгейм

--- first.py
+++ first.py
@@ -1,1 +1,19 @@
-print('My first git-repo')
+import pygame
+from random import randint
+if __name__ == '__main__':
+    size = width, height = 601, 601
+    screen = pygame.display.set_mode(size)
+    clock = pygame.time.Clock()
+    screen.fill((0, 0, 0))
+    running = True
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEMOTION:
+                pygame.draw.circle(screen, (randint(0, 255), randint(0, 255), randint(0, 255)), event.pos, 20)
+        pygame.display.flip()
+        clock.tick(100)
+
+    pygame.quit()
